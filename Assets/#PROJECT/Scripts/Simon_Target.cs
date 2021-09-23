7d774bf9fa2da8031a9cab3b8a4430f4b0137d52
elisabetta-boop <elisabetta.saporetti@yahoo.it>
2021-09-23 13:37:20 +0200

    om

--- Assets/#PROJECT/Scripts/Simon_Target.cs
+++ Assets/#PROJECT/Scripts/Simon_Target.cs
@@ -13,6 +13,9 @@ public class Simon_Target : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.avoidancePriority = Random.Range(1,100);
+        agent.speed = Random.Range(1f,6f);
+
     }
 
     // Update is called once per frame
@@ -20,40 +23,6 @@ public class Simon_Target : MonoBehaviour
     {
         agent.SetDestination(player.position);
     }
-    // private NavMeshAgent agent;
-    // public List<Target_Point> targetPoints = new List<Target_Point>();
-    // public int indexNextDestination = 0;
-    // private Vector3 actualDestination;
-
-    // void Start()
-    // {
-    //     agent = GetComponent<NavMeshAgent>();
-    //     agent.avoidancePriority = Random.Range(1,100);
-    //     agent.speed = Random.Range(1f,6f);
-    //     NextDestination();
-    // }
-
-    
-    // void Update()
-    // {
-    //     if(agent.remainingDistance <= agent.stoppingDistance)
-    //     {
-    //         NextDestination();
-    //     }
-    // }
-    // private void NextDestination()
-    // {
-    //     actualDestination = targetPoints[indexNextDestination].GivePoint();
-    //     agent.SetDestination(actualDestination);
-    //     indexNextDestination++;
-    //     if(indexNextDestination >= targetPoints.Count)
-    //     {
-    //         indexNextDestination =0;
-    //     }
-                
-    //     }
-        
-    
     
     private void OnDrawGizmos() 
     {
